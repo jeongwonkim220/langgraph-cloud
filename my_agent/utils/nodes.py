a47a1beb924ea4d10876be48ccc7d60c9821ae92
jeongwonkim220 <jw0220.kim@lge.com>
2024-08-16 14:20:19 +0900

    Update nodes.py

--- my_agent/utils/nodes.py
+++ my_agent/utils/nodes.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from my_agent.utils.tools import tools
 from langgraph.prebuilt import ToolNode
 
@@ -8,7 +9,11 @@ from langgraph.prebuilt import ToolNode
 @lru_cache(maxsize=4)
 def _get_model(model_name: str):
     if model_name == "openai":
-        model = ChatOpenAI(temperature=0, model_name="gpt-4o")
+        #model = ChatOpenAI(temperature=0, model_name="gpt-4o")
+        model = AzureChatOpenAI(model_name = "gpt-4o-2024-05-13", temperature = 0.0,
+                                api_version="2023-05-15",
+                                azure_endpoint="https://dev.dxengws.apim.lgedx.biz/shared-5000")
+        
     elif model_name == "anthropic":
         model =  ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
     else:
