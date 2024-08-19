20cb4f0e839205343fd59eda8f2212b00a9da884
jeongwonkim220 <jw0220.kim@lge.com>
2024-08-19 11:04:23 +0900

    Update agent2.py

--- my_agent/agent2.py
+++ my_agent/agent2.py
@@ -47,6 +47,22 @@ code_gen_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        error : Binary flag for control flow to indicate whether test error was tripped
+        messages : With user question, error messages, reasoning
+        generation : Code solution
+        iterations : Number of tries
+    """
+
+    error: str
+    messages: List
+    generation: str
+    iterations: int
+
 
 # Data model
 class code(BaseModel):
